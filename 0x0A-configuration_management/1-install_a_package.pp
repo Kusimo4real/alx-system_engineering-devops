cdcf74a31a2024eb2f85ee9ba82436ea46739578
Kusimo4real <kusimosemiu@gmail.com>
2024-06-29 09:41:51 +0100

    trying

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
 #install flask
+#version must be 2.1.0
 
-exec { 'install flask':
-command => 'pip3 install flask==2.1.0',
-path    => '/usr/bin/',
-unless  => 'pip3 list | grep flask',
+package { 'flask':
+ensure   => '2.1.0'
+provider => 'pip3'
 }
+
