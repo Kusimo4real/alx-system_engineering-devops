e6f23d7c187a28fe85bb42f2710b17433414dcf7
Kusimo4real <kusimosemiu@gmail.com>
2024-06-29 09:12:45 +0100

    trying

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,7 @@
 #version must be 2.1.0
 
 exec { 'install flask':
-command => pip3 install Flask==2.1.0,
+command => 'pip3 install Flask==2.1.0',
 path 	=> ['/usr/bin/'],
 unless  => '/usr/bin/test -f /usr/local/lib/python2.1.0/dist-packages/flask/app.py,
 }
