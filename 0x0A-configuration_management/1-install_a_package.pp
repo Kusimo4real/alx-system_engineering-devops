09bc2b8bbcdebc7a7f053f9b7d4f2dedabb719f0
Kusimo4real <kusimosemiu@gmail.com>
2024-06-29 09:30:10 +0100

    trying

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,7 @@
 #version must be 2.1.0
 
 exec { 'install flask':
-command => 'pip3 install Flask==2.1.0',
+command => 'pip3 install flask==2.1.0',
 path    => '/usr/bin/',
 unless  => 'pip3 list | grep flask',
 }
